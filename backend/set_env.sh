961a39b86c9f5ee1d7b12ea6720266d6ce003cad
Mohamed-Mostafa69 <Mohammed.Mostafaa69@gmail.com>
2022-12-25 02:17:50 +0200

    rm

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -1,9 +1,15 @@
 export NODE_ENV=production
 export VERSION=1
 export TYPEORM_CONNECTION=postgres
+<<<<<<< HEAD
 export TYPEORM_MIGRATIONS_DIR=./src/migrations
 export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
 export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+=======
+export TYPEORM_MIGRATIONS_DIR=./migrations
+export TYPEORM_ENTITIES=./modules/domain/*/.entity.js
+export TYPEORM_MIGRATIONS=./migrations/*.js
+>>>>>>> parent of db8578b (locally production)
 export TYPEORM_HOST=localhost
 export TYPEORM_PORT=5532
 export TYPEORM_USERNAME=postgres
